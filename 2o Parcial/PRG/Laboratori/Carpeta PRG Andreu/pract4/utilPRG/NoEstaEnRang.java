5b1ed5847be9ccb652e7c16c3c0f70b559410dfc
Andrac <56803936+andrac275@users.noreply.github.com>
2020-04-08 10:08:46 +0000

    Add files via upload

---
+++ 2o Parcial/PRG/Laboratori/Carpeta PRG Andreu/pract4/utilPRG/NoEstaEnRang.java
@@ -0,0 +1,14 @@
+package pract4.utilPRG;
+
+
+/**
+ * Write a description of class NoEstaEnRang here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class NoEstaEnRang{
+    public NoEstaEnRang(){}
+    //public NoEstaEnRang(String msg){super(msg);}
+
+}
